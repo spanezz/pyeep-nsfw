239fce79b98b3004a489840f72e54e060ab6dcd5
Enrico Zini <enrico@enricozini.org>
2023-04-04 19:04:53 +0200

    Create one component per actuator

--- stim/toy.py
+++ stim/toy.py
@@ -15,12 +15,12 @@ log = logging.getLogger(__name__)
 
 
 class NewDevice(Message):
-    def __init__(self, *, dev: buttplug.client.client.Device, **kwargs):
+    def __init__(self, *, actuator: "Actuator", **kwargs):
         super().__init__(**kwargs)
-        self.dev = dev
+        self.actuator = actuator
 
     def __str__(self):
-        return f"new device: {self.dev.name}"
+        return f"new device: {self.actuator.actuator._device.name} {self.actuator.name}"
 
 
 class ScanRequest(Message):
@@ -35,6 +35,63 @@ class SetPower(Message):
         self.power = power
 
 
+class Actuator(pyeep.aio.AIOComponent):
+    """
+    Component driving an actuator
+    """
+    def __init__(self, *, actuator: buttplug.client.client.Actuator, sample_rate: int = 20, **kwargs):
+        super().__init__(**kwargs)
+        self.actuator = actuator
+
+        # Queue of intensities (from 0 to 1) to be played
+        self.pattern_queue: deque[float] = deque()
+
+        # Sample rate of pattern_queue
+        self.sample_rate: int = 20
+
+        self.frame_nsecs: int = int(round(1_000_000_000 / self.sample_rate))
+
+    async def run(self):
+        await self.process_messages()
+
+    async def process_messages(self):
+        while True:
+            msg = await self.next_message()
+            match msg:
+                case Shutdown():
+                    break
+                # case ScanRequest():
+                #     await self.client.start_scanning()
+                #     scanning = True
+                case SetPower():
+                    if msg.actuator == self.actuator:
+                        await msg.actuator.command(msg.power)
+
+    # async def play_pattern(self):
+    #     last_frame = time.time_ns() / self.frame_nsecs
+    #     old: int = 0
+    #     while not self.shutting_down:
+    #         # print(f"tick cq={len(self.command_queue)}", end=" ")
+    #         if self.pattern_queue:
+    #             new = self.pattern_queue.popleft()
+    #             if new != old:
+    #                 # print(new)
+    #                 await self.send_command(new)
+    #                 old = new
+    #             else:
+    #                 # print("same")
+    #                 pass
+    #         else:
+    #             # print("empty")
+    #             pass
+
+    #         last_frame += 1
+    #         target_time = last_frame * self.frame_nsecs
+    #         cur_time = time.time_ns()
+    #         if target_time > cur_time:
+    #             await asyncio.sleep((target_time - cur_time) / 1_000_000_000)
+
+
 class Toys(pyeep.aio.AIOComponent):
     def __init__(self, client_name: str, iface: str, **kwargs):
         kwargs.setdefault("name", "toys")
@@ -43,14 +100,31 @@ class Toys(pyeep.aio.AIOComponent):
         self.connector = buttplug.WebsocketConnector(iface, logger=self.logger)
         self.devices_seen: set[buttplug.client.client.Device] = set()
 
+    def _new_device(self, dev: buttplug.client.client.Device):
+        name = f"bp_dev{dev.index}"
+
+        for a in dev.actuators:
+            actuator = self.hub.app.add_component(Actuator, name=f"{name}_act{a.index}", actuator=a)
+            self.send(NewDevice(actuator=actuator))
+
+        # for a in dev.linear_actuators:
+        #     print(f"* Linear actuator {a.description} {a.__class__.__name__}")
+
+        # for a in dev.rotatory_actuators:
+        #     print(f"* Rotatory actuator: {a.description} {a.__class__.__name__}")
+
+        # for s in dev.sensors:
+        #     value = await s.read()
+        #     print(f"* Sensor: {s.description} {s.__class__.__name__}: {value}")
+
     async def run(self):
         scanning = False
         await self.client.connect(self.connector)
         try:
-            # Send initially known devices
+            # Create components for initially known devices
             for dev in self.client.devices.values():
                 self.devices_seen.add(dev)
-                self.send(NewDevice(dev=dev))
+                self._new_device(dev)
 
             while True:
                 msg = await self.next_message(timeout=0.2)
@@ -61,15 +135,17 @@ class Toys(pyeep.aio.AIOComponent):
                     case ScanRequest():
                         await self.client.start_scanning()
                         scanning = True
-                    case SetPower():
-                        await msg.actuator.command(msg.power)
+                    # case SetPower():
+                    #     await msg.actuator.command(msg.power)
                     case None:
                         pass
 
                 for dev in self.client.devices.values():
                     if dev not in self.devices_seen:
+                        # Create components for newly discovered devices
                         self.devices_seen.add(dev)
-                        self.send(NewDevice(dev=dev))
+                        self._new_device(dev)
+
         finally:
             if scanning:
                 await self.client.stop_scanning()
@@ -126,87 +202,41 @@ class ToyPlayer:
                 await asyncio.sleep((target_time - cur_time) / 1_000_000_000)
 
 
-class Toy:
-    """
-    Abstract interface to a toy
-    """
-    def __init__(self):
-        # Queue of intensities (from 0 to 1) to be played
-        self.pattern_queue: deque[float] = deque()
-
-        # Sample rate of pattern_queue
-        self.sample_rate: int = 20
-
-        self.frame_nsecs: int = int(round(1_000_000_000 / self.sample_rate))
-
-        self.shutting_down = False
-
-        # Callable notified of every command sent to the toy
-        self.notify_command: Callable[[str], None] | None = None
-
-    def shutdown(self):
-        self.shutting_down = True
-
-    async def play_pattern(self):
-        last_frame = time.time_ns() / self.frame_nsecs
-        old: int = 0
-        while not self.shutting_down:
-            # print(f"tick cq={len(self.command_queue)}", end=" ")
-            if self.pattern_queue:
-                new = self.pattern_queue.popleft()
-                if new != old:
-                    # print(new)
-                    await self.send_command(new)
-                    old = new
-                else:
-                    # print("same")
-                    pass
-            else:
-                # print("empty")
-                pass
-
-            last_frame += 1
-            target_time = last_frame * self.frame_nsecs
-            cur_time = time.time_ns()
-            if target_time > cur_time:
-                await asyncio.sleep((target_time - cur_time) / 1_000_000_000)
-
-
-class MockToy(Toy):
-    """
-    Mock toy, doing nothing
-    """
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc, tb):
-        log.info("shutting down...")
-        self.shutdown()
-
-    async def start(self):
-        self.pattern_queue.clear()
-
-    def print_device_info(self):
-        print("mock toy device")
-
-    async def send_command(self, power: float):
-        cmd = f"Power:{power:.2f}"
-        if self.notify_command:
-            self.notify_command(cmd)
-        else:
-            print(f"mock toy send command {cmd!r}")
-
-
-class ButtplugToy(Toy):
-    """
-    Toy controlled via buttplug/intiface engine
-    """
-    def __init__(self, dev: buttplug.client.client.Device):
-        super().__init__()
-        self.dev = dev
-        self.actuator = 0
-
-    async def send_command(self, power: float):
-        if self.notify_command:
-            self.notify_command(f"Power:{power:.2f}")
-        await self.dev.actuators[0].command(power)
+# class MockToy(Toy):
+#     """
+#     Mock toy, doing nothing
+#     """
+#     async def __aenter__(self):
+#         return self
+#
+#     async def __aexit__(self, exc_type, exc, tb):
+#         log.info("shutting down...")
+#         self.shutdown()
+#
+#     async def start(self):
+#         self.pattern_queue.clear()
+#
+#     def print_device_info(self):
+#         print("mock toy device")
+#
+#     async def send_command(self, power: float):
+#         cmd = f"Power:{power:.2f}"
+#         if self.notify_command:
+#             self.notify_command(cmd)
+#         else:
+#             print(f"mock toy send command {cmd!r}")
+#
+#
+# class ButtplugToy(Toy):
+#     """
+#     Toy controlled via buttplug/intiface engine
+#     """
+#     def __init__(self, dev: buttplug.client.client.Device):
+#         super().__init__()
+#         self.dev = dev
+#         self.actuator = 0
+#
+#     async def send_command(self, power: float):
+#         if self.notify_command:
+#             self.notify_command(f"Power:{power:.2f}")
+#         await self.dev.actuators[0].command(power)
