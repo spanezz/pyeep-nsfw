905b5caca4bd2f01c1b987144262cdd858b8977e
Enrico Zini <enrico@enricozini.org>
2023-04-22 15:10:15 +0200

    Turned old CncRecever into an input, and fixed socket leak

--- stim/cnc.py
+++ stim/cnc.py
@@ -7,14 +7,19 @@ from pathlib import Path
 import pyeep.aio
 from pyeep.app import Message, Shutdown
 
+from .inputs import Input, InputSetActive
+
 
 class CncCommand(Message):
     def __init__(self, *, command: str, **kwargs):
         super().__init__(**kwargs)
         self.command = command
 
+    def __str__(self):
+        return super().__str__() + f"(command={self.command})"
 
-class CncReceiver(pyeep.aio.AIOComponent):
+
+class CncInput(Input, pyeep.aio.AIOComponent):
     def __init__(self, *, path: Path | None = None, **kwargs):
         kwargs.setdefault("name", "cnc")
         super().__init__(**kwargs)
@@ -23,11 +28,18 @@ class CncReceiver(pyeep.aio.AIOComponent):
         else:
             self.path = path
 
+        self.active = False
+
+    @pyeep.aio.export
+    def is_active(self) -> bool:
+        return self.active
+
     async def run(self):
         # Poll socket to connect
         while True:
             try:
                 reader, writer = await asyncio.open_unix_connection(self.path)
+                break
             except ConnectionRefusedError:
                 task_msg = await self.next_message(timeout=0.5)
                 if isinstance(task_msg, Shutdown):
@@ -42,14 +54,18 @@ class CncReceiver(pyeep.aio.AIOComponent):
                 return_when=asyncio.FIRST_COMPLETED)
 
             if task_msg in done:
-                match task_msg.result():
+                match (msg := task_msg.result()):
                     case Shutdown():
                         break
+                    case InputSetActive():
+                        if msg.input == self:
+                            self.active = msg.value
                 task_msg = asyncio.create_task(self.next_message())
 
             if task_line in done:
                 data = json.loads(task_line.result())
                 if data["value"] != 0:
                     # print(data)
-                    self.send(CncCommand(command=data["name"]))
+                    if self.active:
+                        self.send(CncCommand(command=data["name"]))
                 task_line = asyncio.create_task(reader.readline())
