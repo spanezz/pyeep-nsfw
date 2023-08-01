dd65cad23d0c8a4a242d916888bae44b59e790bf
Enrico Zini <enrico@enricozini.org>
2023-08-01 19:16:18 +0200

    Refactored pattern player by splitting channels; incomplete config load; UI tweaks

--- stim/outputs/pattern.py
+++ stim/outputs/pattern.py
@@ -1,64 +1,68 @@
 from __future__ import annotations
 
-from typing import Type
+from typing import Any, Type
 
 import jack
 import numpy
 
 from pyeep.component.controller import ControllerWidget
 from pyeep.component.aio import AIOComponent
-from pyeep.component.base import export, check_hub
+from pyeep.component.base import check_hub, export
 from pyeep.component.jack import JackComponent
-from pyeep.gtk import GLib, Gtk
-from pyeep.messages import Message, Shutdown
+from pyeep.gtk import Gtk
+from pyeep.messages import Shutdown, Configure
 from pyeep.midisynth import SineWave
 from pyeep.outputs.base import OutputController
 
 from ..output import Output
 
 
-class Pattern:
-    def __init__(self):
+class Channel:
+    def __init__(
+            self, *,
+            name: str,
+            osc_freq: float = 1000,
+            lfo_freq: float = 1,
+            min_level: float = 0,
+            max_level: float = 1):
+        self.name = name
         self.rate: int
+
+        self.osc: SineWave
+        self.osc_freq = osc_freq
+
+        self.lfo: SineWave
+        self.lfo_freq = lfo_freq
+
+        self.min_level = min_level
+        self.max_level = max_level
 
     def set_rate(self, rate: int):
         self.rate = rate
-
-    def make_envelopes(self, frames: int) -> tuple[numpy.ndarray, numpy.ndarray]:
-        # TODO: pattern generation for envelopes
-        dummy = numpy.full(frames, 0.5, dtype=numpy.float64)
-        return dummy, dummy
-
-
-class Pan(Pattern):
-    def __init__(self, freq: float, val_min: float = 0.1):
-        super().__init__()
-        self.freq = freq
-        self.val_min = val_min
-        self.lfo: SineWave
-
-    def set_rate(self, rate: int):
-        super().set_rate(rate)
+        self.osc = SineWave(self.rate)
         self.lfo = SineWave(self.rate)
 
-    def make_envelopes(self, frames: int) -> tuple[numpy.ndarray, numpy.ndarray]:
+    def make_envelope(self, frames: int) -> numpy.ndarray:
         lfo = numpy.zeros(frames)
-        self.lfo.wave(lfo, self.freq)
+        self.lfo.wave(lfo, self.lfo_freq)
 
-        left = numpy.clip(0.5 + lfo / 2, self.val_min, 1)
-        right = numpy.clip(0.5 - lfo / 2, self.val_min, 1)
+        span = self.max_level - self.min_level
+        return self.min_level + span / 2 + lfo * span / 2
 
-        return left, right
+    def synth(self, frames: int, array: numpy.ndarray):
+        envelope = self.make_envelope(frames)
+        array.fill(0)
+        self.osc.synth(array, self.osc_freq, envelope)
 
 
-class Pan2(Pattern):
+class Pan2:
     def __init__(
             self, *,
-            lfo_freq_left: float,
-            lfo_freq_right: float,
+            lfo_freq_left: float = 1,
+            lfo_freq_right: float = 1,
             min_level_left: float = 0.1,
             min_level_right: float = 0.1):
-        super().__init__()
+        self.rate: int
         self.lfo_freq_left = lfo_freq_left
         self.lfo_freq_right = lfo_freq_right
         self.min_level_left = min_level_left
@@ -67,7 +71,7 @@ class Pan2(Pattern):
         self.lfo_right: SineWave
 
     def set_rate(self, rate: int):
-        super().set_rate(rate)
+        self.rate = rate
         self.lfo_left = SineWave(self.rate)
         self.lfo_right = SineWave(self.rate)
 
@@ -89,11 +93,8 @@ class Pan2(Pattern):
 class PatternPlayer(Output, JackComponent, AIOComponent):
     def __init__(self, frequency: float = 1000.0, **kwargs):
         super().__init__(rate=0, **kwargs)
-        self.sine_left: SineWave
-        self.sine_right: SineWave
-        self.frequency_left = frequency
-        self.frequency_right = frequency
-        self.pattern: Pattern | None = None
+        self.left = Channel(name="pattern_L")
+        self.right = Channel(name="pattern_R")
 
     def get_output_controller(self) -> Type[OutputController]:
         return PatternOutputController
@@ -101,60 +102,74 @@ class PatternPlayer(Output, JackComponent, AIOComponent):
     def set_jack_client(self, jack_client: jack.Client):
         super().set_jack_client(jack_client)
         self.rate = jack_client.samplerate
-        self.sine_left = SineWave(self.rate)
-        self.sine_right = SineWave(self.rate)
+        self.left.set_rate(self.rate)
+        self.right.set_rate(self.rate)
         self.outport_l = self.jack_client.outports.register('pattern_L')
         # print(self.jack_client.inports)
         # print(self.jack_client.outports)
         # print(self.jack_client.get_ports(name_pattern="Built-in Audio Analog Stereo:", is_audio=True, is_input=True))
         self.outport_r = self.jack_client.outports.register('pattern_R')
         # TODO: autoconnect to previously connected ports
-        # self.set_pattern(Pan(freq=3))
-        # TODO: restore settings from last run
-        self.set_pattern(Pan2(lfo_freq_left=2, lfo_freq_right=1, min_level_right=0.4))
         self.outport_l.connect("Built-in Audio Analog Stereo:playback_FL")
         self.outport_r.connect("Built-in Audio Analog Stereo:playback_FR")
 
-    def set_pattern(self, pattern: Pattern):
-        self.pattern = pattern
-        self.pattern.set_rate(self.rate)
-
     def jack_process(self, frames: int) -> None:
-        if self.pattern is None:
-            return
-
-        envelope_l, envelope_r = self.pattern.make_envelopes(frames)
-
-        array_l = self.outport_l.get_array()
-        array_l.fill(0)
-        self.sine_left.synth(array_l, self.frequency_left, envelope_l)
-
-        array_r = self.outport_r.get_array()
-        array_r.fill(0)
-        self.sine_right.synth(array_r, self.frequency_right, envelope_r)
+        self.left.synth(frames, self.outport_l.get_array())
+        self.right.synth(frames, self.outport_r.get_array())
 
     @export
     def setup(
             self, *,
-            freq_left: float,
-            freq_right: float,
-            lfo_freq_left: float,
-            lfo_freq_right: float,
-            min_level_left: float,
-            min_level_right: float):
-        self.frequency_left = freq_left
-        self.frequency_right = freq_right
-        self.pattern.lfo_freq_left = lfo_freq_left
-        self.pattern.lfo_freq_right = lfo_freq_right
-        self.pattern.min_level_left = min_level_left
-        self.pattern.min_level_right = min_level_right
+            freq_left: float | None = None,
+            freq_right: float | None = None,
+            lfo_freq_left: float | None = None,
+            lfo_freq_right: float | None = None,
+            min_level_left: float | None = None,
+            min_level_right: float | None = None):
+        if freq_left is not None:
+            self.left.osc_freq = freq_left
+        if freq_right is not None:
+            self.right.osc_freq = freq_right
+        if lfo_freq_left is not None:
+            self.left.lfo_freq = lfo_freq_left
+        if lfo_freq_right is not None:
+            self.right.lfo_freq = lfo_freq_right
+        if min_level_left is not None:
+            self.left.min_level = min_level_left
+        if min_level_right is not None:
+            self.right.min_level = min_level_right
+
+    @check_hub
+    def load_config(self, config: dict[str, Any]):
+        print("PATTERN LC")
+        super().load_config(config)
+        kwargs: dict[str, Any] = {}
+        for arg in ("freq_left", "freq_right", "lfo_freq_left", "lfo_freq_right", "min_level_left", "min_level_right"):
+            if (val := config.get(arg)) is not None:
+                kwargs[arg] = val
+        if kwargs:
+            print("PATTERN LC", kwargs)
+            self.setup(**kwargs)
+
+    @check_hub
+    def get_config(self) -> dict[str, Any]:
+        res = super().get_config()
+        res["frequency_left"] = self.frequency_left
+        res["frequency_right"] = self.frequency_right
+        res["lfo_freq_left"] = self.pattern.lfo_freq_left
+        res["lfo_freq_right"] = self.pattern.lfo_freq_right
+        res["min_level_left"] = self.pattern.min_level_left
+        res["min_level_right"] = self.pattern.min_level_right
+        return res
 
     async def run(self) -> None:
         while True:
-            msg = await self.next_message()
-            match msg:
+            match (msg := await self.next_message()):
                 case Shutdown():
                     break
+                case Configure():
+                    print("PATTERN CONFIG", msg.config)
+                    self.load_config(msg.config)
 
 
 class PatternOutputController(OutputController):
@@ -171,7 +186,7 @@ class PatternOutputController(OutputController):
         # min level right
 
         self.freq_left = Gtk.Adjustment(
-                value=150,
+                value=self.output.left.osc_freq,
                 lower=100,
                 upper=2000,
                 step_increment=10,
@@ -180,7 +195,7 @@ class PatternOutputController(OutputController):
         # self.freq_left.connect("value_changed", self.on_freq_left)
 
         self.freq_right = Gtk.Adjustment(
-                value=150,
+                value=self.output.right.osc_freq,
                 lower=100,
                 upper=2000,
                 step_increment=10,
@@ -188,7 +203,7 @@ class PatternOutputController(OutputController):
                 page_size=0)
 
         self.lfo_freq_left = Gtk.Adjustment(
-                value=1,
+                value=self.output.left.lfo_freq,
                 lower=0,
                 upper=10,
                 step_increment=0.1,
@@ -197,7 +212,7 @@ class PatternOutputController(OutputController):
         # self.freq_left.connect("value_changed", self.on_freq_left)
 
         self.lfo_freq_right = Gtk.Adjustment(
-                value=1,
+                value=self.output.right.lfo_freq,
                 lower=0,
                 upper=10,
                 step_increment=0.1,
@@ -205,19 +220,19 @@ class PatternOutputController(OutputController):
                 page_size=0)
 
         self.min_level_left = Gtk.Adjustment(
-                value=0,
+                value=self.output.left.min_level,
                 lower=0,
                 upper=1,
-                step_increment=0.01,
+                step_increment=0.05,
                 page_increment=0.1,
                 page_size=0)
         # self.min_level_left.connect("value_changed", self.on_power_min)
 
         self.min_level_right = Gtk.Adjustment(
-                value=0,
+                value=self.output.right.min_level,
                 lower=0,
                 upper=1,
-                step_increment=0.01,
+                step_increment=0.05,
                 page_increment=0.1,
                 page_size=0)
 
@@ -225,7 +240,7 @@ class PatternOutputController(OutputController):
                 value=1,
                 lower=0,
                 upper=1,
-                step_increment=0.01,
+                step_increment=0.05,
                 page_increment=0.1,
                 page_size=0)
         # self.min_level_left.connect("value_changed", self.on_power_min)
@@ -234,7 +249,7 @@ class PatternOutputController(OutputController):
                 value=1,
                 lower=0,
                 upper=1,
-                step_increment=0.01,
+                step_increment=0.05,
                 page_increment=0.1,
                 page_size=0)
 
