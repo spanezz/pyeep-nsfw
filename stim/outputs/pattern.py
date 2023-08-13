4a8e03394c9310d0f7f6055733abcdde36730299
Enrico Zini <enrico@enricozini.org>
2023-08-13 18:16:05 +0200

    Use a PowerOutputController for patterns, and use power as main volume

--- stim/outputs/pattern.py
+++ stim/outputs/pattern.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Type
+from typing import Any, Callable, Type
 
 import jack
 import numpy
@@ -14,19 +14,24 @@ from pyeep.messages import Shutdown, Configure
 from pyeep.midisynth import SineWave
 from pyeep.outputs.base import OutputController
 
-from ..output import Output
+from ..output import PowerOutput, PowerOutputController
 
 
 class Channel:
     def __init__(
             self, *,
             name: str,
+            label: str,
+            volume: float = 1.0,
             osc_freq: float = 1000,
             lfo_freq: float = 1,
             min_level: float = 0,
             max_level: float = 0.3):
         self.name = name
+        self.label = label
         self.rate: int
+
+        self.volume = volume
 
         self.osc: SineWave
         self.osc_freq = osc_freq
@@ -47,19 +52,24 @@ class Channel:
         self.lfo.wave(lfo, self.lfo_freq)
 
         span = self.max_level - self.min_level
-        return self.min_level + span / 2 + lfo * span / 2
+        return (self.min_level + span / 2 + lfo * span / 2) * self.volume
 
     def synth(self, frames: int, array: numpy.ndarray):
-        envelope = self.make_envelope(frames)
         array.fill(0)
+
+        if self.volume == 0.0:
+            # Skip computing waveforms if volume is 0
+            return
+
+        envelope = self.make_envelope(frames)
         self.osc.synth(array, self.osc_freq, envelope)
 
 
-class PatternPlayer(Output, JackComponent, AIOComponent):
+class PatternPlayer(PowerOutput, JackComponent, AIOComponent):
     def __init__(self, frequency: float = 1000.0, **kwargs):
         super().__init__(rate=0, **kwargs)
-        self.left = Channel(name="pattern_L")
-        self.right = Channel(name="pattern_R")
+        self.left = Channel(name="pattern_L", label="Left/A")
+        self.right = Channel(name="pattern_R", label="Right/B")
 
     def get_output_controller(self) -> Type[OutputController]:
         return PatternOutputController
@@ -81,6 +91,11 @@ class PatternPlayer(Output, JackComponent, AIOComponent):
     def jack_process(self, frames: int) -> None:
         self.left.synth(frames, self.outport_l.get_array())
         self.right.synth(frames, self.outport_r.get_array())
+
+    @export
+    def set_power(self, power: float):
+        self.left.volume = power
+        self.right.volume = power
 
     @export
     def setup(
@@ -143,7 +158,79 @@ class PatternPlayer(Output, JackComponent, AIOComponent):
                     self.load_config(msg.config)
 
 
-class PatternOutputController(OutputController):
+class ChannelController:
+    def __init__(self, channel_name: str):
+        self.channel_name = channel_name
+
+        self.freq = Gtk.Adjustment(
+                value=1000.0,
+                lower=100,
+                upper=2000,
+                step_increment=10,
+                page_increment=100,
+                page_size=0)
+
+        self.lfo_freq = Gtk.Adjustment(
+                value=1.0,
+                lower=0,
+                upper=10,
+                step_increment=0.1,
+                page_increment=1,
+                page_size=0)
+
+        self.min_level = Gtk.Adjustment(
+                value=0.0,
+                lower=0,
+                upper=1,
+                step_increment=0.05,
+                page_increment=0.1,
+                page_size=0)
+
+        self.max_level = Gtk.Adjustment(
+                value=1.0,
+                lower=0,
+                upper=1,
+                step_increment=0.01,
+                page_increment=0.1,
+                page_size=0)
+
+    def connect_value_changed(self, cb: Callable[[Gtk.Adjustement], None]):
+        self.freq.connect("value_changed", cb)
+        self.lfo_freq.connect("value_changed", cb)
+        self.min_level.connect("value_changed", cb)
+        self.max_level.connect("value_changed", cb)
+
+    def get_setup_kwargs(self) -> dict[str, str]:
+        return {
+            f"freq_{self.channel_name}": self.freq.get_value(),
+            f"lfo_freq_{self.channel_name}": self.lfo_freq.get_value(),
+            f"min_level_{self.channel_name}": self.min_level.get_value(),
+            f"max_level_{self.channel_name}": self.max_level.get_value(),
+        }
+
+    def attach_ui(self, cw: ControllerWidget, x: int, y: int):
+        freq = Gtk.SpinButton(
+            adjustment=self.freq,
+            digits=1)
+        cw.grid.attach(freq, x, y, 2, 1)
+
+        lfo_freq = Gtk.SpinButton(
+            adjustment=self.lfo_freq,
+            digits=1)
+        cw.grid.attach(lfo_freq, x, y + 1, 2, 1)
+
+        min_level = Gtk.SpinButton(
+            adjustment=self.min_level,
+            digits=2)
+        cw.grid.attach(min_level, x, y + 2, 2, 1)
+
+        max_level = Gtk.SpinButton(
+            adjustment=self.max_level,
+            digits=2)
+        cw.grid.attach(max_level, x, y + 3, 2, 1)
+
+
+class PatternOutputController(PowerOutputController):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -151,77 +238,10 @@ class PatternOutputController(OutputController):
         # TODO: LFO shape left (sine, saw, square)
         # TODO: LFO shape right (sine, saw, square)
 
-        self.freq_left = Gtk.Adjustment(
-                value=self.output.left.osc_freq,
-                lower=100,
-                upper=2000,
-                step_increment=10,
-                page_increment=100,
-                page_size=0)
-        self.freq_left.connect("value_changed", self.on_change)
-
-        self.freq_right = Gtk.Adjustment(
-                value=self.output.right.osc_freq,
-                lower=100,
-                upper=2000,
-                step_increment=10,
-                page_increment=100,
-                page_size=0)
-        self.freq_right.connect("value_changed", self.on_change)
-
-        self.lfo_freq_left = Gtk.Adjustment(
-                value=self.output.left.lfo_freq,
-                lower=0,
-                upper=10,
-                step_increment=0.1,
-                page_increment=1,
-                page_size=0)
-        self.lfo_freq_left.connect("value_changed", self.on_change)
-
-        self.lfo_freq_right = Gtk.Adjustment(
-                value=self.output.right.lfo_freq,
-                lower=0,
-                upper=10,
-                step_increment=0.1,
-                page_increment=1,
-                page_size=0)
-        self.lfo_freq_right.connect("value_changed", self.on_change)
-
-        self.min_level_left = Gtk.Adjustment(
-                value=self.output.left.min_level,
-                lower=0,
-                upper=1,
-                step_increment=0.05,
-                page_increment=0.1,
-                page_size=0)
-        self.min_level_left.connect("value_changed", self.on_change)
-
-        self.min_level_right = Gtk.Adjustment(
-                value=self.output.right.min_level,
-                lower=0,
-                upper=1,
-                step_increment=0.05,
-                page_increment=0.1,
-                page_size=0)
-        self.min_level_right.connect("value_changed", self.on_change)
-
-        self.max_level_left = Gtk.Adjustment(
-                value=0.3,
-                lower=0,
-                upper=1,
-                step_increment=0.01,
-                page_increment=0.1,
-                page_size=0)
-        self.max_level_left.connect("value_changed", self.on_change)
-
-        self.max_level_right = Gtk.Adjustment(
-                value=0.3,
-                lower=0,
-                upper=1,
-                step_increment=0.01,
-                page_increment=0.1,
-                page_size=0)
-        self.max_level_right.connect("value_changed", self.on_change)
+        self.left = ChannelController(channel_name="left")
+        self.left.connect_value_changed(self.on_change)
+        self.right = ChannelController(channel_name="right")
+        self.right.connect_value_changed(self.on_change)
 
         self.auto_apply = Gio.SimpleAction.new_stateful(
                 name=self.name.replace("_", "-") + "-auto-apply",
@@ -229,49 +249,10 @@ class PatternOutputController(OutputController):
                 state=GLib.Variant.new_boolean(False))
         self.hub.app.gtk_app.add_action(self.auto_apply)
 
-    # @check_hub
-    # def set_paused(self, paused: bool):
-    #     """
-    #     Enter/exit pause mode
-    #     """
-    #     super().set_paused(paused)
-
-    #     if paused:
-    #         self.output.set_power(0)
-    #     else:
-    #         power = self.power.get_value() / 100.0
-    #         self.output.set_power(power)
-
-    # @check_hub
-    # def emergency_stop(self):
-    #     self.power.set_value(0)
-    #     self.power_levels.clear()
-    #     super().emergency_stop()
-
-    # @check_hub
-    # def receive(self, msg: Message):
-    #     match msg:
-    #         case SetGroupPower():
-    #             if self.in_group(msg.group):
-    #                 self.set_source_power(msg.src, msg.power)
-    #         case IncreaseGroupPower():
-    #             if self.in_group(msg.group):
-    #                 match msg.amount:
-    #                     case PowerAnimation():
-    #                         self.power_animator.start(msg.amount)
-    #         case _:
-    #             super().receive(msg)
-
     def on_apply(self, button):
         self.output.setup(
-            freq_left=self.freq_left.get_value(),
-            freq_right=self.freq_right.get_value(),
-            lfo_freq_left=self.lfo_freq_left.get_value(),
-            lfo_freq_right=self.lfo_freq_right.get_value(),
-            min_level_left=self.min_level_left.get_value(),
-            min_level_right=self.min_level_right.get_value(),
-            max_level_left=self.max_level_left.get_value(),
-            max_level_right=self.max_level_right.get_value(),
+            **self.left.get_setup_kwargs(),
+            **self.right.get_setup_kwargs(),
         )
 
     def on_change(self, element):
@@ -282,59 +263,22 @@ class PatternOutputController(OutputController):
     def build(self) -> ControllerWidget:
         cw = super().build()
 
-        cw.grid.attach(Gtk.Label(label="Left"), 1, 2, 2, 1)
-        cw.grid.attach(Gtk.Label(label="Right"), 3, 2, 2, 1)
-        cw.grid.attach(Gtk.Label(label="Freq"), 0, 3, 1, 1)
-        cw.grid.attach(Gtk.Label(label="LFO"), 0, 4, 1, 1)
-        cw.grid.attach(Gtk.Label(label="Min"), 0, 5, 1, 1)
-        cw.grid.attach(Gtk.Label(label="Max"), 0, 6, 1, 1)
+        cw.grid.attach(Gtk.Label(label=self.output.left.label), 1, 4, 2, 1)
+        cw.grid.attach(Gtk.Label(label=self.output.right.label), 3, 4, 2, 1)
+        cw.grid.attach(Gtk.Label(label="Freq"), 0, 5, 1, 1)
+        cw.grid.attach(Gtk.Label(label="LFO"), 0, 6, 1, 1)
+        cw.grid.attach(Gtk.Label(label="Min"), 0, 7, 1, 1)
+        cw.grid.attach(Gtk.Label(label="Max"), 0, 8, 1, 1)
 
-        freq_left = Gtk.SpinButton(
-            adjustment=self.freq_left,
-            digits=1)
-        cw.grid.attach(freq_left, 1, 3, 2, 1)
-
-        freq_right = Gtk.SpinButton(
-            adjustment=self.freq_right,
-            digits=1)
-        cw.grid.attach(freq_right, 3, 3, 2, 1)
-
-        lfo_freq_left = Gtk.SpinButton(
-            adjustment=self.lfo_freq_left,
-            digits=1)
-        cw.grid.attach(lfo_freq_left, 1, 4, 2, 1)
-
-        lfo_freq_right = Gtk.SpinButton(
-            adjustment=self.lfo_freq_right,
-            digits=1)
-        cw.grid.attach(lfo_freq_right, 3, 4, 2, 1)
-
-        min_level_left = Gtk.SpinButton(
-            adjustment=self.min_level_left,
-            digits=2)
-        cw.grid.attach(min_level_left, 1, 5, 2, 1)
-
-        min_level_right = Gtk.SpinButton(
-            adjustment=self.min_level_right,
-            digits=2)
-        cw.grid.attach(min_level_right, 3, 5, 2, 1)
-
-        max_level_left = Gtk.SpinButton(
-            adjustment=self.max_level_left,
-            digits=2)
-        cw.grid.attach(max_level_left, 1, 6, 2, 1)
-
-        max_level_right = Gtk.SpinButton(
-            adjustment=self.max_level_right,
-            digits=2)
-        cw.grid.attach(max_level_right, 3, 6, 2, 1)
+        self.left.attach_ui(cw, 1, 5)
+        self.right.attach_ui(cw, 3, 5)
 
         pulse = Gtk.Button(label="Apply")
         pulse.connect("clicked", self.on_apply)
-        cw.grid.attach(pulse, 0, 7, 1, 1)
+        cw.grid.attach(pulse, 0, 9, 1, 1)
 
         decay = Gtk.ToggleButton(label="Auto apply")
         decay.set_action_name("app." + self.auto_apply.get_name())
-        cw.grid.attach(decay, 1, 7, 1, 1)
+        cw.grid.attach(decay, 1, 9, 1, 1)
 
         return cw
